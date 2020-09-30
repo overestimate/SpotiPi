f54b56bce6db3ebca935cc3465066748fd4ec9b7
Tyler Lawrence <58041173+overestimate@users.noreply.github.com>
2020-09-30 14:13:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email web-playback';
+  var scope = 'user-read-private user-read-email streaming';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
